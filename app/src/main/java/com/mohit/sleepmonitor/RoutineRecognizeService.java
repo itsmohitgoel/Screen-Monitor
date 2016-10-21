06b0b3c6309a26f0e3e8a1de816954fa94f16629
itsmohitgoel <itsmohitgoel@gmail.com>
2016-10-21 22:52:49 +0530

    get Detected Activities from intent received

--- app/src/main/java/com/mohit/sleepmonitor/RoutineRecognizeService.java
+++ app/src/main/java/com/mohit/sleepmonitor/RoutineRecognizeService.java
@@ -3,6 +3,11 @@ package com.mohit.sleepmonitor;
 import android.app.IntentService;
 import android.content.Intent;
 
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.List;
+
 /**
  * Created by Mohit on 21-10-2016.
  */
@@ -17,6 +22,11 @@ public class RoutineRecognizeService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-
+        // if intent contains activity recognizition data, then
+        // retrive list of detected activities
+        if (ActivityRecognitionResult.hasResult(intent)) {
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            List<DetectedActivity> probableActivities = result.getProbableActivities();
+        }
     }
 }
