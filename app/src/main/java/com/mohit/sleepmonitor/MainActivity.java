deddea735de03d798c34f7e0b24aff5716e7f66f
itsmohitgoel <itsmohitgoel@gmail.com>
2016-10-21 22:31:07 +0530

    Add GoogleApiClient's callback interfaces

--- app/src/main/java/com/mohit/sleepmonitor/MainActivity.java
+++ app/src/main/java/com/mohit/sleepmonitor/MainActivity.java
@@ -1,13 +1,35 @@
 package com.mohit.sleepmonitor;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mApiClient; // hold reference to api client
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
